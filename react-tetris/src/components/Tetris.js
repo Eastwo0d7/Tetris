f48a25993327a39b018e4a7553ec116f74910edd
Brett LeBaron <brett.lebaron@gmail.com>
2019-11-12 21:03:50 -0500

    starting to create tetris pieces

---
+++ react-tetris/src/components/Tetris.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { createStage} from '../gameHelpers';
+
+//Components
+import Stage from './Stage';
+import Display from './Display';
+import StartButton from './StartButton';
+
+const Tetris = () => {
+    return (
+        <div>
+          <Stage stage={createStage()}/>
+          <aside>
+              <div>
+                <Display text="score" />
+                <Display text="rows" />
+                <Display text="level" />
+              </div>
+              <StartButton />
+            </aside>  
+        </div>
+    );
+};
+
+export default Tetris;
